968f9a1fab1438aa9c82b82e56d9e4d5d67d1ce9
DavidTarlamazyan <erika.azatyan.y@tumo.org>
2023-07-12 13:53:04 +0400

    Session 1

---
+++ Session 1/pie.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+categories = ['Category A', 'Category B', 'Category C', 'Category D']
+percetages = [25, 30, 15, 30]
+
+explode = [0, 0.1, 0, 0]
+colors = ['red', 'green', 'blue', 'yellow']
+
+plt.pie(percetages, explode = explode, labels = categories, colors = colors, shadow = True)
+plt.title("Percetages Division")
+plt.legend(categories)
+plt.show()
+
