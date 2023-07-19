229a91533718968da8f1ef369262cd13ff5b98e3
DavidTarlamazyan <davit.t@tumo.org>
2023-07-19 12:58:02 +0400

    Final v1

--- Final_Project/room1.py
+++ Final_Project/room1.py
@@ -38,12 +38,34 @@ while True:
 	rgb_frame = cv2.cvtColor(frame, cv2.BGR2RGB)
 
 	for (x,y,w,h) in face_rects:
-		face_roi = gray[y:y+h, x:x+w])
-		face_encoding = face_recognition.face_encodings(rhb_frame. [(y, x+w, y+h, x)])[0]
-	matched_names = []
-	distances = face_recognition.face_distance(known_face_encodings, face encoding)
-	min_distances = min(distances)
-	if min_distances < 0.5:
-		matched_names.append(known_face_names[np.argmin(distances)])
+		face_roi = gray[y:y+h, x:x+w]
+		face_encoding = face_recognition.face_encodings(rgb_frame[(y, x+w, y+h, x)])[0]
+		matched_names = []
+		distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+		min_distances = min(distances)
+		if min_distances < 0.5:
+			matched_names.append(known_face_names[np.argmin(distances)])
+		rectangle_color = {0, 255, 0} if matched_names and "room1" in [access.lower() for access in known_face_access[np.argmin(distance)]] else (0,0,255)
+		cv2.rectangle(frame, (x,y), (x+w, y+h), rectangle_color, 3)
+		if matched_names and "room1" in [access.lower() for access in known_face_access[np.argmin(distance)]]:
+			access_text = f"{matched_names[0]} - Access Granted - Room 1"
+			access_text_color = (0, 255, 0)
+		else:
+			access_text = "Unknown" if not matched_names else f"{matched_names[0]} - Access Denied - Room 1"
+			access_text_color = (0, 0, 255)
+		(text_width, text_height) = cv2.getTextSize(access_text, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+		text_x = x+(w - text_width) // 2
+		text_y = y-10 if (y-10) > text_height else y + h + 10
+		
+		cv2.putText(frame, access_text, (text_x, text_y), cv2.FONTHERSHEY_SIMPLEX, 0.6, access_text_color, 1)
+
+	cv2.imshow("Room 1 Face Detector", frame)
+
+	if cv2.waitKey(1) == 27:
+		break
+
+vap.release()
+cv2.destroyAllWindows()
+
 
 	
